62abe363261699ff7dd9729d2bb08a26f0656c78
tim dumm <timdumm@hotmail.com>
2020-03-08 20:22:44 -0700

    updated for .NetCore 3

--- XeroOAuth2Sample/Startup.cs
+++ XeroOAuth2Sample/Startup.cs
@@ -119,8 +119,8 @@ namespace XeroOAuth2Sample
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddControllersWithViews();
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         private static Func<TokenValidatedContext, Task> OnTokenValidated()
@@ -153,17 +153,19 @@ namespace XeroOAuth2Sample
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-
+            app.UseRouting();
+            app.UseCors();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
             app.UseAuthentication();
+            app.UseAuthorization();
 
-            app.UseMvc(routes =>
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
+                endpoints.MapControllerRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
